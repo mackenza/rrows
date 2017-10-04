7ec1ad603bee0f55d8891f5a9008d42db77f8760
mackenza <a.mackenzie@gmail.com>
2017-10-04 08:32:02 -0400

    work on ws client

--- rapidresponse.js
+++ rapidresponse.js
@@ -1,2 +1,9 @@
 const soap = require('soap');
 
+function userMaint(url, username, password, action, params) {
+    const wsSecurity = new soap.WSSecurity(username, password);
+    const rrClient = soap.createClient(url,function(err, client) {
+        client.setSecurity(wsSecurity);
+        
+    })
+}
