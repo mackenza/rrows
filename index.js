df6925c814dd7f3c4d0049120139beb0add43f81
Andrew MacKenzie <a.mackenzie@gmail.com>
2017-10-04 11:12:33 +0000

    added dotenv

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const rrws = require('./rapidresponse')
+require('dotenv').config();
+const rrws = require('./rapidresponse');
+
+const RRServerURL = process.env.RRSERVERURL;
+const RRUsername = process.env.RRUSERNAME;
+const RRPassword = process.env.RRPASSWORD;
+
