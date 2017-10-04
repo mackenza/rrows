c8478f10f979d792a4bc189fe8c190513ef5e1ae
Andrew MacKenzie <a.mackenzie@gmail.com>
2017-10-04 15:49:53 -0400

    ws seems to work

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 require('dotenv').config();
-const rrws = require('./rapidresponse');
+//const rrws = require('./rapidresponse.js');
 
 const RRServerURL = process.env.RRSERVERURL;
 const RRUsername = process.env.RRUSERNAME;
 const RRPassword = process.env.RRPASSWORD;
 
+const soap = require('soap');
+
+function userMaint(url, username, password, action, params) {
+    const wsSecurity = new soap.WSSecurity(username, password);
+    const sid = {
+        Name: "AM User Maint",
+        Scope: "Public"
+    }
+    const esi = {
+        Script: sid,
+        Arguments: null
+    }
+    const args = {
+        esi: esi
+    }
+    soap.createClient(url,function(err, client) {
+        client.setSecurity(wsSecurity);
+        client.ExecuteScript(args, function(err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(result);
+            }
+        });
+    });
+}
+userMaint(RRServerURL, RRUsername, RRPassword, 'add', '');
--- rapidresponse.js
+++ rapidresponse.js
@@ -2,8 +2,12 @@ const soap = require('soap');
 
 function userMaint(url, username, password, action, params) {
     const wsSecurity = new soap.WSSecurity(username, password);
-    const rrClient = soap.createClient(url,function(err, client) {
+    soap.createClient(url,function(err, client) {
         client.setSecurity(wsSecurity);
-        
-    })
+        return client.describe();
+    });
+}
+
+module.exports = {
+    userMaint = userMaint
 }
